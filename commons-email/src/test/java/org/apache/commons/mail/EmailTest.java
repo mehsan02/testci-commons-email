454652c67155043dd2f7ff413e43e8efa67213dd
muminahehsan <muminahehsan@192.168.4.39>
2021-04-08 18:31:06 -0400

    EmailTest.java file created and set up to write test methods

---
+++ commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,31 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+	
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org",
+			"acbdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception{
+		email = new EmailConcrete();
+	}
+	
+
+}
+	
